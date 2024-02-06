0adbf97a609d8b89ff93bb6c685224356467b6e0
Thijs van den Anker <thijs@bitsnbolts.nl>
2024-02-06 10:15:50 +0100

    Fix description

--- plugin/open-env.lua
+++ plugin/open-env.lua
@@ -3,4 +3,4 @@ vim.api.nvim_create_user_command("OpenEnv", function()
 end, {})
 
 -- Bind <leader>oe to the command
-vim.api.nvim_set_keymap("n", "<leader>oe", ":OpenEnv<CR>", { noremap = true, silent = true, desc = "open .env file" })
+vim.api.nvim_set_keymap("n", "<leader>oe", ":OpenEnv<CR>", { noremap = true, silent = true, desc = "Open .env file" })
